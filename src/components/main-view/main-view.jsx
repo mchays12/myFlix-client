dace59dce6dd41e8867376ecb6362231db656595
mchays12 <mchays12@gmail.com>
2023-06-14 13:59:03 -0400

    updated to connect to api

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,34 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieView } from "../movie-view/movie-view";
 import { MovieCard } from "../movie-card/movie-card";
 
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "John Wick Chapter 4",
-      image: "https://m.media-amazon.com/images/M/MV5BOTI1MDA5OTEtNWExMi00NGYwLThmMjUtMDhmNDQ4YTg1NzZlXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_FMjpg_UX1000_.jpg",
-      genre: "action",
-      director: "Chad Stahelski"
-    },
-    {
-      id: 2,
-      title: "John Wick Chapter 3",
-      image: "https://m.media-amazon.com/images/M/MV5BYTE0MTE2ZDAtNDNlOC00OGQwLTkxOTUtNzhlMjA1MWJkZWEyXkEyXkFqcGdeQXVyOTkwMTc4ODQ@._V1_.jpg",
-      genre: "action",
-      director: "Chad Stahelski"
-    },
-    {
-      id: 3,
-      title: "John Wick Chapter 2",
-      image: "https://m.media-amazon.com/images/I/81zur+RwWHL._AC_UF894,1000_QL80_.jpg",
-      genre: "action",
-      director: "Chad Stahelski"
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://myflixappmatthew.herokuapp.com/")
+      .then((reponse) => response.json())
+      .then((data) => {
+        const moviesFromApi = datadocs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.Title,
+            director: "", /*pretty sure I need an array*/
+            genre: "" /*pretty sure I need an array*/
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,7 @@
+//Import the PropTypes library
+import PropTypes from "prop-types";
+
+//BookCard function component
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div onClick={() => { onMovieClick(movie) }}
@@ -5,4 +9,23 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+
+//Define all the props constraints for the BookCard
+MovieCard.PropTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      birthday: PropTypes.string.isRequired,
+      deathday: PropTypes.string
+    }),
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired
+    })
+  }).isRequired,
+  onMovieClick: PropTypesfunc.isRequired
 };
